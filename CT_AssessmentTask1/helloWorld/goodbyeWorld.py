1d997149c394a40e10ebb7959396cfe6bf94fdb8
Torquiosegurl <torquiosegurl@gmail.com>
2026-09-24 13:46:48 +1000

    Delete CT_AssessmentTask1 directory

--- CT_AssessmentTask1/helloWorld/goodbyeWorld.py
+++
@@ -1,9 +0,0 @@
-#This is Assessmenttask2 that prints 'Hello World' and then pressing enter prints 'Goodbye Person'
-print("----Start of Output ---------------------------")
-print(" ")
-print("Hello World")
-input("------> Program paused - press enter to continue") #Makes a pause in the program
-print("Goodbye World")
-print(" ")
-print("----End of Output -----------------------------")
-
